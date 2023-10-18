c3259d8a5ccd1af228973f2004daf825d880e503
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-17 18:08:38 -0700

    new coodee

--- focal/dice-roller.js
+++ focal/dice-roller.js
@@ -3,6 +3,7 @@ const numDice = process.argv[2];
 if (!numDice || isNaN(numDice) || numDice < 1){
   console.log("Please provide a valid positive number of dide to roll.")
 } else {
-  const rolls = array.from({length: numDice}, () = > Math.floor(math.random() * 6) + 1);
+  const rolls = Array.from({length: numDice}, () => Math.floor(Math.random() * 6) + 1);
   console.log(`Rolled ${numDice} dice: ${rolls.join(", ")}`);
 }
+
